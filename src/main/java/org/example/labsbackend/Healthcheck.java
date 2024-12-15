88be0a50ac64f7ede0697dc0f7406abf04efcd0c
Lamarrrk <londonbridge1602@gmail.com>
2024-12-16 00:46:48 +0200

    Fix main controller

--- src/main/java/org/example/labsbackend/Healthcheck.java
+++ src/main/java/org/example/labsbackend/Healthcheck.java
@@ -3,20 +3,24 @@ package org.example.labsbackend;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/healthcheck")
 public class Healthcheck {
-
-    @GetMapping
+    @GetMapping("/")
+    public RedirectView redirectToHealthcheck() {
+        return new RedirectView("/healthcheck");
+    }
+    @GetMapping("/healthcheck")
     public Map<String, Object> healthCheck() {
         Map<String, Object> response = new HashMap<>();
         response.put("status", "OK");
         response.put("timestamp", LocalDateTime.now());
         return response;
-
     }
 }
+
